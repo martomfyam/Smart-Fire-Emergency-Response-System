727b8ae1795de114efc62a21a84245708793d1ea
martomfyam <38663386+martomfyam@users.noreply.github.com>
2020-07-08 15:07:22 +0300

    added temp sensor

--- project.ino
+++ project.ino
@@ -19,12 +19,15 @@
    Digital Pin 3: DC gear reduction motor attached to the lock. (Or a motor controller or a solenoid or other unlocking mechanisim.)
    Digital Pin 4: Red LED. 
    Digital Pin 5: Green LED.
+   Digital Pin 6: yellow LED.
    Digital Pin 7: GSM (7).
    Digital Pin 8: GSM (8).
-   Digital Pin 11: IR receiver.
-   Analog pin A0: piezo buzzer (passive). to Digital Pin 10
+   Digital Pin 10: piezo buzzer
+   Digital Pin 11: Button.
+   Digital Pin 13: flame sensor.
    Analog pin A1: temp sensor
    Analog pin A2: smoke sensor
+   
     
 
  */
@@ -37,7 +40,9 @@
 SoftwareSerial SIM900(7, 8); //SIM900 Tx & Rx is connected to Arduino #7 & #8
 Servo lockMotor;
 
-
+int buttonState = 0; 
+String button;
+int buttonv = 1;
 String userMessage = "There is a fire incident in your home";
 String userNumber = "+254792020946";
 String marshalMessage = "Fire incident at House number 6,| location -> googlemaps.com";
@@ -48,13 +53,18 @@ int initialTime;
 int delayTime;
 String textMessage;
 int temp;
+int flame_pin = HIGH ; // state of sensor
+
 int smokeA2 = A2;
 int pinTemp = A1;   //This is where our Output data goes
 
 const int lockpin = 3;   // Configure servo pin
 const int redLED = 4;              // Status red LED
 const int greenLED = 5;            // Status green LED
+const int yellowLED = 6;
 const int piezoPin = 10;  //PIEZO PIN
+const int buttonPin = 11; 
+const int flame_sensor_pin = 13 ;// initializing pin 12 as the sensor output pin
 
 void setup()
 {
@@ -63,13 +73,19 @@ void setup()
   pinMode(pinTemp, INPUT); 
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
   pinMode(piezoPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode ( flame_sensor_pin , INPUT );  
   
   lockMotor.write(60);
   digitalWrite(greenLED, HIGH);        // Green LED on, everything is go.
   delay(1000);
   digitalWrite(greenLED, LOW);
-  digitalWrite(redLED, HIGH);        // Green LED on, everything is go.
+  digitalWrite(yellowLED, HIGH);        // Green LED on, everything is go.
+  delay(1000);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(redLED, HIGH);        // Yellow LED on, everything is go. //change the format they are lighting
   delay(1000);
   digitalWrite(redLED, LOW);
 
@@ -98,19 +114,28 @@ void setup()
 void loop()
 {
   
- // sendSMS(message, phoneNumber);
- // delay(100000);
   
   Serial.print("alarm status is: ");
   Serial.println(alarmStatus);
   Serial.print(analogRead(smokeA2));
   Serial.println(temp);
-  
+
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+
+   if (buttonState == HIGH ) {  
+  alarmOff();
+  }
+    
  if(SIM900.available()>0){
   receiveSMS();  
 }
 
 if (alarmStatus == "set" ){
+  digitalWrite(yellowLED, HIGH);        // Yellow LED on, everything is go.
+  delay(200);
+  digitalWrite(yellowLED, LOW);
    Serial.println("alarm status");
   alarmOn();
 }
@@ -125,15 +150,30 @@ if (alarmStatus == "set" ){
   noTone(piezoPin);
   
   } else {
- if(analogRead(smokeA2)> 195){
+  
+  digitalWrite(greenLED, HIGH);        // Green LED on, everything is go.
+  delay(100);
+  digitalWrite(greenLED, LOW);
+ if(analogRead(smokeA2)> 250){
   smokefunction();
  }
  
 //check temp reading and compare with its threshold
  temp = analogRead(pinTemp);    //Read the analog pin
-temp = temp * 0.48828125;   // convert output (mv) to readable celcius
-if (temp >50) {
+ temp = temp * 0.48828125;   // convert output (mv) to readable celcius
+//float voltage = temp * 3.3;
+//voltage /= 1024.0;
+//temp = (voltage - 0.5) * 100;
+if (temp >36) {
   tempfunction(temp);
+}
+
+
+flame_pin = digitalRead ( flame_sensor_pin ) ; // reading from the sensor
+if (flame_pin == LOW ) // applying condition
+{
+Serial.println ( " FLAME , FLAME , FLAME " ) ;
+flameSensor();
 }
 
 
@@ -158,7 +198,7 @@ if (temp >50) {
 //   Serial.println(smokevalue);
    
     //tone( pin number, frequency in hertz, duration in milliseconds);
-    String message = "Their is an incoming fire alarm. Kindly act accordingly. You can cancel within 30sec by replying with OFF.";
+    String message = "There is an incoming fire alarm. Kindly act accordingly. You can cancel within 30sec by replying with OFF.";
     sendSMS(message, userNumber);
     initialTime = millis(); //take time when alarm request started
     alarmOn();
@@ -181,22 +221,33 @@ if (temp >50) {
 void tempfunction(int temperature){
   
   // request for an alarm alert
-  
+  String message = "There is an incoming fire alarm. Kindly act accordingly. You can cancel within 30sec by replying with 'OFF'.";
   Serial.print("Temperature value is: ");
   Serial.println(temperature);
  // Serial.println("C");  //print the temperature status
  //  String Message = "Their is an incoming fire alarm. Kindly act accordingly. You can cancel within 30sec.";
- //  sendSMS(message, userNumber);
- // initialTime = millis(); //take time when alarm request started
- // alarmOn();
+   sendSMS(message, userNumber);
+  initialTime = millis(); //take time when alarm request started
+  alarmOn();
 //  tone(piezoPin, 1000, 200);
-  delay(1000); 
+//  delay(1000); 
 //  noTone(piezoPin);
   
 }
 
 
 
+
+
+
+
+
+void flameSensor(){
+    String message = "There is an incoming fire alarm. Kindly act accordingly. You can cancel within 30sec by replying with 'OFF'.";
+    sendSMS(message, userNumber);
+    initialTime = millis(); //take time when alarm request started
+    alarmOn();
+}
 
 
 
@@ -215,26 +266,39 @@ void alarmOn(){
   delayTime = now - initialTime;   //in seconds
   //send a message to the user
   while (alarmStatus == "set"){
+    buttonState = digitalRead(buttonPin);
+//  Serial.print("button state is: ");
+//  Serial.println(buttonState);
+   if (buttonState == HIGH ) {  //not working
+  alarmOff();
+  }
   if(SIM900.available()>0){
   receiveSMS();  
   }
+  digitalWrite(yellowLED, HIGH);        // Yellow LED on, everything is go.
+  delay(100);
+  digitalWrite(yellowLED, LOW);
   now = millis();
   delayTime = now - initialTime;   //in seconds
   Serial.print("delay time is: ");
   Serial.println(delayTime);
-  while (delayTime > cutoffTime ){
+  if (delayTime > cutoffTime ){
     
     
-    triggerDoorUnlock();  //open door
-    tone(piezoPin, 1000, 200);
+    //triggerDoorUnlock();  //open door
+    //tone(piezoPin, 1000, 200);
     sendSMS(userMessage, userNumber);
+
     sendSMS(marshalMessage, marshalNumber);
+
+    triggerDoorUnlock();  //open door
     //send message to the owners ->sendSMS("message here",recpeits);
     //send message to the firefighter ->sendSMS("map link",recpeits);
     alarmStatus = "on";
   //  delay(2000);
     delayTime = 0;
     //check if i can add continue/break so that code dont proceed to temp
+    break;
   }
   delay(2000);
   }
@@ -255,6 +319,7 @@ void alarmOff(){
   alarmStatus = "off";
   triggerDoorlock(); 
   noTone(piezoPin);
+  
 }
 
 
@@ -324,7 +389,10 @@ void triggerDoorlock(){
    // triggerDoorlock();        //open door by calling doorunlock function
    //    initialTime = millis();
    //    alarmOn();
-   alarmStatus = "on"; //change coz door wouldnt open
+   if(alarmStatus == "off"){   //if alarm is on dont interfere with it
+   initialTime = millis(); //take time when alarm request started
+   alarmStatus = "set"; //change coz door wouldnt open
+   }
     textMessage = "";   
   }
 
@@ -346,6 +414,28 @@ void triggerDoorlock(){
 
 
 
+//// Function that sends SMS
+//void sendSMS(String text, String Number){
+//  
+//  SIM900.println("AT"); //Handshaking with SIM900
+//  updateSerial();
+//  delay(1000);
+//  SIM900.println("AT+CMGF=1"); // Configuring TEXT mode
+//  updateSerial();
+//  delay(1000);
+//  SIM900.println("AT+CMGS=\""+Number+"\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+//  updateSerial();
+//  delay(1000);
+//  SIM900.print(text); //text content
+//  updateSerial();
+//  delay(1000);
+//  SIM900.write(26);
+//  delay(1000);
+//}
+
+
+
+
 
 
 
@@ -362,6 +452,7 @@ void sendSMS(String text, String Number){
   SIM900.print(text); //text content
   updateSerial();
   SIM900.write(26);
+  delay(4000);
 }
 
 
